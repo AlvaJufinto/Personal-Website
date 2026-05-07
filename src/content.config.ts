b62bc17284bc78003757259b96f3f279381b8b1f
Alva Jufinto <stanislaus.alva.jufinto@gmail.com>
2026-05-07 19:17:22 +0700

    add: new blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,21 +4,21 @@ import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-			isDraft: z.boolean().default(false),
-			topics: z.array(z.string()).default([]).optional(),
-			language: z.string().default("en").optional(),
-		}),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+      isDraft: z.boolean().optional().default(false),
+      topics: z.array(z.string()).default([]).optional(),
+      language: z.string().default("en").optional(),
+    }),
 });
 
 export const collections = { blog };
