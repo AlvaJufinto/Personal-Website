527b2ba4094879286f1f54b71290d52b80d24f4f
Alva Jufinto <stanislaus.alva.jufinto@gmail.com>
2026-05-10 00:07:32 +0700

    enhance: tech stack and footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,125 @@
-/** @format */
+/ @format */;
 
 // Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// You can import this data from anywhere in your site by using the import keyword.
 
 export const SITE_TITLE = "Alva's";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Stanislaus Alva Jufinto Personal Website ";
+export const TECH_STACK = [
+  {
+    type: "frontend",
+    stack: [
+      {
+        name: "React",
+        logo: "https://cdn.worldvectorlogo.com/logos/react-2.svg",
+      },
+      {
+        name: "Next.js",
+        logo: "https://cdn.worldvectorlogo.com/logos/next-js.svg",
+      },
+      {
+        name: "Inertia.js",
+        logo: "https://avatars.githubusercontent.com/u/47703742?s=280&v=4",
+      },
+      {
+        name: "Flutter",
+        logo: "https://cdn.worldvectorlogo.com/logos/flutter.svg",
+      },
+      {
+        name: "Dart",
+        logo: "https://cdn.worldvectorlogo.com/logos/dart.svg",
+      },
+      {
+        name: "Tailwind CSS",
+        logo: "https://cdn.worldvectorlogo.com/logos/tailwindcss.svg",
+      },
+      {
+        name: "SCSS",
+        logo: "https://cdn.worldvectorlogo.com/logos/sass-1.svg",
+      },
+      {
+        name: "TypeScript",
+        logo: "https://cdn.worldvectorlogo.com/logos/typescript.svg",
+      },
+      {
+        name: "JavaScript",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsLRmUi1H6nCJpjUgCE6SRymXbR1mWgeFu6A&s",
+      },
+      {
+        name: "Zustand",
+        logo: "https://repository-images.githubusercontent.com/180328715/fca49300-e7f1-11ea-9f51-cfd949b31560",
+      },
+    ],
+  },
+  {
+    type: "backend",
+    stack: [
+      {
+        name: "Node.js",
+        logo: "https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg",
+      },
+      {
+        name: "Express.js",
+        logo: "https://cdn.worldvectorlogo.com/logos/express-109.svg",
+      },
+      {
+        name: "Laravel",
+        logo: "https://cdn.worldvectorlogo.com/logos/laravel-2.svg",
+      },
+      {
+        name: "PHP",
+        logo: "https://cdn.freebiesupply.com/logos/large/2x/php-1-logo-png-transparent.png",
+      },
+      {
+        name: "Python",
+        logo: "https://cdn.worldvectorlogo.com/logos/python-5.svg",
+      },
+    ],
+  },
+  {
+    type: "database",
+    stack: [
+      {
+        name: "MySQL",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGGT_pnMHJdps9fGjcDaFSqcfFxO2E7BQk4g&s",
+      },
+      {
+        name: "MongoDB",
+        logo: "https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg",
+      },
+      {
+        name: "Firebase",
+        logo: "https://cdn.worldvectorlogo.com/logos/firebase-1.svg",
+      },
+      {
+        name: "Supabase",
+        logo: "https://cdn.worldvectorlogo.com/logos/supabase.svg",
+      },
+      {
+        name: "SQLite",
+        logo: "https://cdn.worldvectorlogo.com/logos/sqlite.svg",
+      },
+    ],
+  },
+  {
+    type: "tools",
+    stack: [
+      {
+        name: "Google Gemini",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYM-_oCfjiysWdSUR-8tzX8fUEtcdTo1naJg&s",
+      },
+      {
+        name: "Cypress",
+        logo: "https://cdn.worldvectorlogo.com/logos/cypress-1.svg",
+      },
+      {
+        name: "Sanity",
+        logo: "https://cdn.worldvectorlogo.com/logos/sanity.svg",
+      },
+      {
+        name: "Storybook",
+        logo: "https://cdn.worldvectorlogo.com/logos/storybook-1.svg",
+      },
+    ],
+  },
+];
